487323bad0aa2e287c95e2ce17519449f419523d
BeginerLord <messismark0@gmail.com>
2025-04-09 23:05:21 -0500

    feat: Add useLoginHook for handling user login with success and error notifications

---
+++ src/hooks/Auth/useLoginHook.ts
@@ -0,0 +1,28 @@
+import { login } from "@/service/Auth";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+export const useLoginHook = () => {
+  const { mutate: loginMutation, isPending } = useMutation({
+    mutationFn: login,
+
+    onSuccess: (data) => {
+      // Notificar al usuario del éxito
+      toast.success("¡Inicio de sesión exitoso!");
+
+      // Opcional: Redirigir al usuario o realizar otras acciones
+      console.log("Datos del usuario:", data);
+    },
+
+    onError: (error) => {
+      // Manejo de errores y notificación
+      const errorMessage =
+        error instanceof Error
+          ? error.message
+          : "Ocurrió un error desconocido al iniciar sesión.";
+      toast.error(errorMessage);
+    },
+  });
+
+  return { loginMutation, isPending };
+};
